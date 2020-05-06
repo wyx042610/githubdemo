5848e9c36e84ad37a2d657be7800dadf59e7e87e
wangyixing <zxcv042610>
2020-05-06 11:38:23 +0800

    Initial commit

---
+++ src/main/java/com/offcn/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.offcn.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello(){
+        return "hello spring boot";
+    }
+}
